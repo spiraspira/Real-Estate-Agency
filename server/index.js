daf3edad4125a65ef27304d1d5885c53b06f3595
spiraspira <zabornik5350@gmail.com>
2024-01-05 16:37:03 +0300

    feat: add bd config and sequelize code

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./models/index");
+
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ alter: true });
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();

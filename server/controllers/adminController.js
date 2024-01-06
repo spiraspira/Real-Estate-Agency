684a585a256245944bde805791755b644da83bee
spiraspira <zabornik5350@gmail.com>
2024-01-06 15:47:30 +0300

    feat: update adminController

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,4 @@
-const { Admin } = require('../database/models');
+const { Admin } = require('../models/models');
 
 class AdminController {
   async getAll(req, res) {
@@ -35,6 +35,31 @@ class AdminController {
       return res.sendStatus(500);
     }
   }
+
+
+  async getProfile(req, res) {
+    const id = req.adminId;
+
+    if (isNaN(id)) {
+      return res.sendStatus(400);
+    }
+
+    try {
+      const user = await Admin.findOne({
+        where: { id: id },
+        attributes: { exclude: ["password"] },
+      });
+
+      if (user == null) {
+        return res.sendStatus(404);
+      }
+
+      return res.json(user);
+    } catch (err) {
+      return res.sendStatus(500);
+    }
+  }
 }
+
 
 module.exports = new AdminController();

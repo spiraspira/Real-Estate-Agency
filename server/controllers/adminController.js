2306df6918304bd57b034f71c2bb3ae608ecab99
spiraspira <zabornik5350@gmail.com>
2024-01-08 18:26:51 +0300

    feat: refactored adminController

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,65 +1,72 @@
 const { Admin } = require('../models/models');
 
 class AdminController {
-  async getAll(req, res) {
-    try {
-      const admins = await Admin.findAll({
-        attributes: { exclude: ['password'] },
-      });
+    async getAll(req, res) {
+        try {
+            const admins = await Admin.findAll({
+                attributes: { exclude: ['password'] },
+            });
 
-      return res.json(admins);
-    } catch (err) {
-      return res.sendStatus(500);
-    }
-  }
+            return res.json(admins);
+        }
+        catch (err) {
+            console.log(err);
 
-  async getOne(req, res) {
-    const { id } = req.params;
-
-    if (isNaN(id)) {
-      return res.sendStatus(400);
+            return res.status(500).json({ error: 'Ошибка сервера' });
+        }
     }
 
-    try {
-      const admin = await Admin.findOne({
-        where: { id: id },
-        attributes: { exclude: ['password'] },
-      });
+    async getOne(req, res) {
+        const { id } = req.params;
 
-      if (admin === null) {
-        return res.sendStatus(404);
-      }
+        if (isNaN(id)) {
+            return res.sendStatus(400);
+        }
 
-      return res.json(admin);
-    } catch (err) {
-      return res.sendStatus(500);
+        try {
+            const admin = await Admin.findOne({
+                where: { id: id },
+                attributes: { exclude: ['password'] },
+            });
+
+            if (admin === null) {
+                return res.sendStatus(404);
+            }
+
+            return res.json(admin);
+        }
+        catch (err) {
+            console.log(err);
+
+            return res.status(500).json({ error: 'Ошибка сервера' });
+        }
     }
-  }
 
+    async getProfile(req, res) {
+        const id = req.adminId;
 
-  async getProfile(req, res) {
-    const id = req.adminId;
+        if (isNaN(id)) {
+            return res.sendStatus(400);
+        }
 
-    if (isNaN(id)) {
-      return res.sendStatus(400);
+        try {
+            const user = await Admin.findOne({
+                where: { id: id },
+                attributes: { exclude: ["password"] },
+            });
+
+            if (user == null) {
+                return res.sendStatus(404);
+            }
+
+            return res.json(user);
+        }
+        catch (err) {
+            console.log(err);
+
+            return res.status(500).json({ error: 'Ошибка сервера' });
+        }
     }
-
-    try {
-      const user = await Admin.findOne({
-        where: { id: id },
-        attributes: { exclude: ["password"] },
-      });
-
-      if (user == null) {
-        return res.sendStatus(404);
-      }
-
-      return res.json(user);
-    } catch (err) {
-      return res.sendStatus(500);
-    }
-  }
 }
-
 
 module.exports = new AdminController();

e43abcb9fe69aceba89c8b71e73c76eb54b65f0b
spiraspira <zabornik5350@gmail.com>
2024-01-08 17:07:56 +0300

    feat: refactored some project files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./models/index");
 const models = require('./models/models')
-const models = require('./models/models')
+const router = require('./routers/index')
 
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
@@ -10,13 +10,15 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(router)
 
 const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync({ alter: true });
         app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-    } catch (e) {
+    }
+    catch (e) {
         console.log(e);
     }
 };

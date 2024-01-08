ce253d165926bf488dcea3d6d6633edee60fc6e2
spiraspira <zabornik5350@gmail.com>
2024-01-08 17:18:20 +0300

    feat: refactored models

--- server/models/models.js
+++ server/models/models.js
@@ -8,9 +8,7 @@ const User = sequelize.define('User', {
     phone: { type: DataTypes.STRING, allowNull: false },
     birthDate: { type: DataTypes.DATE, allowNull: false },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    password: { type: DataTypes.STRING, allowNull: false }
 });
 
 const Property = sequelize.define('Property', {
@@ -18,61 +16,45 @@ const Property = sequelize.define('Property', {
     name: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
     price: { type: DataTypes.INTEGER, allowNull: false },
-    isSold: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    isSold: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false}
 });
 
 const PropertyType = sequelize.define('PropertyType', {
     Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: {type: DataTypes.STRING, allowNull: false},
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
-});
-
-const Sale = sequelize.define('Sale', {
-    Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    name: {type: DataTypes.STRING, allowNull: false}
 });
 
 const Review = sequelize.define('Review', {
     Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     isPositive: {type: DataTypes.BOOLEAN, allowNull: false},
-    description: {type: DataTypes.STRING, allowNull: false},
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    description: {type: DataTypes.STRING, allowNull: false}
 });
 
 const Image = sequelize.define('Image', {
     Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    image: {type: DataTypes.BLOB('long'), allowNull: false},
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    image: {type: DataTypes.BLOB('long'), allowNull: false}
 });
 
 const Admin = sequelize.define('Admin', {
     Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    password: { type: DataTypes.STRING, allowNull: false }
 });
 
 const Contact = sequelize.define('Contact', {
     Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    type: {type: DataTypes.STRING, allowNull: false},
-    value: {type: DataTypes.STRING, allowNull: false},
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
+    value: {type: DataTypes.STRING, allowNull: false}
+});
+
+const ContactType = sequelize.define('ContactType', {
+    Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: {type: DataTypes.STRING, allowNull: false},
 });
 
 const Deal = sequelize.define('Deal', {
     Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     isClosed: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
     isSold: { type: DataTypes.BOOLEAN, allowNull: false},
-    dateCreated: {type: DataTypes.DATE, allowNull: false},
-    dateModified: {type: DataTypes.DATE, allowNull: false}
 });
 
 Property.hasOne(PropertyType);
@@ -87,16 +69,17 @@ User.belongsTo(Review);
 Image.hasOne(Property);
 Property.belongsTo(Image);
 
-Sale.hasOne(Deal);
-Deal.belongsTo(Sale);
+Contact.hasOne(ContactType);
+ContactType.belongsTo(Contact);
 
 module.exports = {
-  User,
-  Property,
-  PropertyType,
-  Deal,
-  Sale,
-  Contact,
-  Image,
-  Review
+    User,
+    Property,
+    PropertyType,
+    Deal,
+    Contact,
+    Image,
+    Review,
+    ContactType,
+    Admin
 };

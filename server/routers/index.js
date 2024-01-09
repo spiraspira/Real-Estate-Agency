6a00c1491354b5f85851ed503b3d601fe7cf4145
spiraspira <zabornik5350@gmail.com>
2024-01-09 17:20:40 +0300

    feat: add index to routers

---
+++ server/routers/index.js
@@ -0,0 +1,16 @@
+const Router = require('express')
+const router = new Router()
+const authRouter = require('./authRouter')
+const propertyRouter = require('./propertyRouter')
+const profileRouter = require('./profileRouter')
+const protectedRouter = require("./protectedRouter");
+const reviewRouter = require("./reviewRouter");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.use('/auth', authRouter)
+router.use('/properties', propertyRouter)
+router.use('/profile', profileRouter)
+router.use('/reviews', reviewRouter)
+router.use("/api", authMiddleware, protectedRouter);
+
+module.exports = router

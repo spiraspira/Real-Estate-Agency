f6df56426ab8e6b5315cc6130643787c373b9435
spiraspira <zabornik5350@gmail.com>
2024-01-09 16:30:23 +0300

    feat: refactored propertyRouter

--- server/routers/propertyRouter.js
+++ server/routers/propertyRouter.js
@@ -4,5 +4,10 @@ const propertyController = require('../controllers/propertyController');
 
 router.get('', propertyController.getCatalogProperties);
 router.get('/:propertyId', propertyController.getCatalogProperty);
+router.get('/:propertyTypeId', propertyController.getCatalogPropertiesByType);
+router.get('/adminpanel/properties', propertyController.getAll);
+router.get('/adminpanel/:propertyId', propertyController.getOne);
+router.post('/create', propertyController.createProperty);
+router.post('/update', propertyController.updateProperty);
 
 module.exports = router;

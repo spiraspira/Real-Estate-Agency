45374457b4b0b279997e758cafefa75ccf7f3868
spiraspira <zabornik5350@gmail.com>
2024-01-10 15:02:38 +0300

    feat: add some files

---
+++ client/src/utils/consts.js
@@ -0,0 +1,4 @@
+export const LOGIN_ROUTE = "/login";
+export const USER_REGISTER_ROUTE = "/register/user";
+export const USER_MAIN_MENU_ROUTE = "/main";
+export const ADMIN_ORDERS_ROUTE = "/admin-main";

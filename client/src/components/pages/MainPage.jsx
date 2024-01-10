c0009e53e6d5f906f0e5e65253428983ae9396cf
spiraspira <zabornik5350@gmail.com>
2024-01-10 18:44:01 +0300

    feat: add mainPage

---
+++ client/src/components/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Footer from './mainPage/Footer';
+import Header from '../header/Header';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />  
+      <Footer />
+    </div>
+  );
+};
+
+export default MainPage;
